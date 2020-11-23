9b3e8de0723436a9fe597cf109a660b292daf33f
James Xing <jamesxuxing@gmail.com>
2020-11-23 21:49:19 +1100

    Add city.csv

--- search/search/spiders/fcl.py
+++ search/search/spiders/fcl.py
@@ -1,11 +1,27 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-:
+import csv
 import scrapy
-
+from scrapy.http import Request
+from scrapy.loader import ItemLoader
+from scrapy.item import Item, Field
 
 class FclSpider(scrapy.Spider):
     name = 'fcl'
     allowed_domains = ['firstclasslearning.co.uk']
-    start_urls = ['http://firstclasslearning.co.uk/']
+    base_url = 'https://www.firstclasslearning.co.uk/find-a-centre?postcode='
+
+    def start_requests(self):
+		with open("city.csv", "rU") as f:
+			reader = csv.DictReader(f)
+			for line in reader:
+				city = line.pop('city')
+				url = base_url + city
+				request = Request(url)
+				request.meta['city'] = city
+				yield request
+
 
     def parse(self, response):
-        pass
+        item = Item()
+		l = ItemLoader(item=item, response=response)
+
