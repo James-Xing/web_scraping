16246dd791f9d0a62205ba657ef087d6a5a96b93
James-Xing <jamesxuxing@gmail.com>
2021-01-03 22:56:23 +1100

    Update amazon_review.py

--- Amazon_review/amazon_review.py
+++ Amazon_review/amazon_review.py
@@ -2,23 +2,58 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import pandas as pd
 
+review_list = []
 
-session = HTMLSession()
+def get_data(url):
 
-url = 'https://www.amazon.com/SanDisk-MicroSDXC-Nintendo-Switch-SDSQXAO-128G-GNCZN/product-reviews/B07KXQX3S3/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
+    session = HTMLSession()
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66'
 
-r = session.get(url, headers = {'user_agent':user_agent})
+    r = session.get(url, headers = {'user_agent':user_agent})
 
-soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-reviews = soup.find_all('div', {'data-hook':'review'})
+    return soup
 
-for review in reviews:
-    reviewer = review.find('span', {'class':'a-profile-name'})
-    rating = review.find('i', {'data-hook':'review-star-rating'})
-    title = review.find('a',{'data-hook':'review-title'})
-    review_data = review.find('span', {'data-hook':'review-date'})
-    print(reviewer, rating, title, review_data)
+def extract_data(soup):
+
+    reviews = soup.find_all('div', {'data-hook':'review'})
+
+    for item in reviews:
+
+        review = {
+            'reviewer': item.find('span', {'class':'a-profile-name'}),
+            'rating': item.find('i', {'data-hook':'review-star-rating'}),
+            'title': item.find('a',{'data-hook':'review-title'}),
+            'review_data': item.find('span', {'data-hook':'review-date'})
+        }
+
+        review_list.append(review) 
+
+    return reviews   
+
+
+for i in range(1, 10):
+    url = f"https://www.amazon.com/Motorola-Unlocked-T-Mobile-MetroPCS-XT2019-2/product-reviews/B083PSRMQ4/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber={i}"
+    soup = get_data(url)
+    extract_data(soup)
+    print(len(review_list))
+    if not soup.find('li', {'class':'a-disabled a-last'}):
+        pass
+    else:
+        break
+
+
+df = pd.DataFrame(review_list)
+
+df.to_csv('amazon_review.csv', index = False)
+    
+
+
+
+        
+
+
+
 
